5e6f00858de37ffe6d32f31ed151dc1e9b7779ad
rajalwaysonthemove <37570664+rajalwaysonthemove@users.noreply.github.com>
2018-07-12 11:33:35 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,23 +1,3 @@
 require('dotenv').config()
 const express = require("express")
-const bodyParser = require("body-parser")
-const morgan = require("morgan");
-const path = require("path");
-const root = process.cwd()
-const app = express();
-const PORT = process.env.PORT
-const ENV = process.env.NODE_ENV
 
-app.use(morgan('tiny'))
-app.use(express.static("public"));
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
-app.use("/api", require("./routes.js"))
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(root, "index.html"))
-})
-
-app.listen(PORT, () => {
-  console.log(`server is running on ${PORT} port in ${ENV} mode`);
-})
